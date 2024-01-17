abd7d11e38609ab18ae91c3cc8c68be7aefc703e
Suphawit <suphawit.won@gmail.com>
2024-01-17 14:53:57 +0700

    Implemented session

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "secret"
 
 connect = sqlite3.connect('database.db')
 connect.executescript(
@@ -27,14 +28,18 @@ def hello_world():
 
 @app.route('/login', methods=['GET', 'POST'])
 def l():
+    if "user" in session:
+        return "<h1>Logged in</h1>"
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
         u = request.form['username']
         p = request.form['password']
         try:
-            udb = sqlite3.connect("database.db").cursor().execute(f'SELECT * FROM users WHERE username = {u}').fetchone()
-            if u == udb.username and p == udb.password:
+            udb = sqlite3.connect("database.db").cursor().execute(f'SELECT * FROM users WHERE username = ? AND password = ?', (u, p)).fetchone()
+            print(udb[1], udb[2])
+            if udb:
+                session["user"] = u
                 return "<h1>Logged in</h1>"
         except:
             return redirect(url_for('l'))
@@ -50,46 +55,53 @@ def r():
         with sqlite3.connect("database.db") as users:
             users.cursor().execute("INSERT INTO users (username, password) VALUES (?, ?)", (u, p))
             users.commit()
+            session["user"] = u
         return redirect(url_for('l'))
     
 
 @app.route('/join', methods=['GET'])
 def join():
-    with sqlite3.connect("database.db") as users:
-        cursor = users.cursor()
-        cursor.execute(
-"""
-INSERT INTO users (username)
-VALUES ("justin")
-"""
-        )
-        users.commit()
-    return "<p>Done!</p>"
+    if "user" in session:
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
+            cursor.execute(
+    """
+    INSERT INTO users (username)
+    VALUES ("justin")
+    """
+            )
+            users.commit()
+        return "<p>Done!</p>"
+    return  redirect(url_for('l'))
 
 @app.route('/leave', methods=['GET', 'POST'])
 def leave():
-    with sqlite3.connect("database.db") as users:
-        cursor = users.cursor()
-        cursor.execute(
-"""
-INSERT INTO leave (username, reason, time)
-VALUES ("justin", "why not", DATE('now'))
-"""
-        )
-        users.commit()
-    return "<p>Done!</p>"
+    if "user" in session:
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
+            cursor.execute(
+    """
+    INSERT INTO leave (username, reason, time)
+    VALUES ("justin", "why not", DATE('now'))
+    """
+            )
+            users.commit()
+        return "<p>Done!</p>"
+    return redirect(url_for('l'))
 
 @app.route('/list', methods=['GET'])
 def list():
-    if len((w := "<ul>" + "".join(f'<li>{r[1]} - AT: {r[3]} REASON: {r[2]}</li>' for r in sqlite3.connect("database.db").cursor().execute('SELECT * FROM leave').fetchall()) + "</ul>")) != len('123456789'):
-        return "<h1>Users</h1>" + w
-    return "<h1>Users</h1>" + "<p>List empty</p>"
+    if "user" in session:
+        if len((w := "<ul>" + "".join(f'<li>{r[1]} - AT: {r[3]} REASON: {r[2]}</li>' for r in sqlite3.connect("database.db").cursor().execute('SELECT * FROM leave').fetchall()) + "</ul>")) != len('123456789'):
+            return "<h1>Leave Requests</h1>" + w
+        return "<h1>Leave Requests</h1>" + "<p>List empty</p>"
+    return redirect(url_for('l'))
 
 @app.route('/users', methods=['GET'])
 def users():
     if len((w := "<ul>" + "".join(f'<li>{r[1]}</li>' for r in sqlite3.connect("database.db").cursor().execute('SELECT * FROM users').fetchall()) + "</ul>")) != len('123456789'):
-        return "<h1>Leave Requests</h1>" + w
-    return "<h1>Leave Requests</h1>" + "<p>List empty</p>"
+        return "<h1>Users</h1>" + w
+    return "<h1>Users</h1>" + "<p>List empty</p>"
         
 
 if __name__ == '__main__':
