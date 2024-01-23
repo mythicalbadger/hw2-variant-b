5d18378057a78417c48932e61204ff5d4b854307
mythicalbadger <jussy12.work@gmail.com>
2024-01-23 12:35:09 +0700

    refactor: naming

--- app.py
+++ app.py
@@ -10,61 +10,66 @@ CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username
 CREATE TABLE IF NOT EXISTS leave (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, reason TEXT NOT NULL, time Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL);
 """)
 
+
 @app.route("/")
-def hello_world():
+def main():
     return "<p>Hello, World!</p>"
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def l():
+def login():
     if "user" in session:
         return "<h1>Logged in</h1>"
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
-        u = request.form['username']
-        p = request.form['password']
+        username = request.form['username']
+        password = request.form['password']
         try:
-            udb = sqlite3.connect("database.db").cursor().execute(f'SELECT * FROM users WHERE username = ? AND password = ?', (u, p)).fetchone()
-            print(udb[1], udb[2])
-            if udb:
-                session["user"] = u
+            user_in_db = sqlite3.connect("database.db").cursor().execute(
+                f'SELECT * FROM users WHERE username = ? AND password = ?', (username, password)).fetchone()
+            print(user_in_db[1], user_in_db[2])
+            if user_in_db:
+                session["user"] = username
                 return "<h1>Logged in</h1>"
         except:
-            return redirect(url_for('l'))
-        return redirect(url_for('l'))
+            return redirect(url_for('login'))
+        return redirect(url_for('login'))
+
 
 @app.route('/logout', methods=['GET'])
-def out():
+def logout():
     session["user"] = None
-    return redirect(url_for('l'))
+    return redirect(url_for('login'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def r():
+def register():
     if request.method == 'GET':
         return render_template('register.html')
     elif request.method == 'POST':
-        u = request.form['username']
-        p = request.form['password']
-        with sqlite3.connect("database.db") as users:
-            users.cursor().execute("INSERT INTO users (username, password) VALUES (?, ?)", (u, p))
-            users.commit()
-            session["user"] = u
-        return redirect(url_for('l'))
-    
+        username = request.form['username']
+        password = request.form['password']
+        with sqlite3.connect("database.db") as db:
+            db.cursor().execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            db.commit()
+            session["user"] = username
+        return redirect(url_for('login'))
+
+
 @app.route('/current_user')
 def current_user():
     user = "None" if session["user"] == None else session["user"]
     if "user" in session:
         return "<h1>" + user + "</h1>"
-    return redirect(url_for('l'))
+    return redirect(url_for('login'))
 
-@app.route('/leave', methods=['GET', 'POST'])
-def leave():
+
+@app.route('/create_leave_request', methods=['GET', 'POST'])
+def create_leave_request():
     if "user" in session:
         if request.method == 'GET':
-            return render_template('leave.html')
+            return render_template('create_leave_request.html')
         elif request.method == 'POST':
             r = request.form['reason']
             with sqlite3.connect("database.db") as users:
@@ -73,31 +78,41 @@ def leave():
         INSERT INTO leave (username, reason, time) VALUES (?, ?, DATE('now'))
     """, (session["user"], r))
                 users.commit()
-            return redirect(url_for('mlist'))
-    return redirect(url_for('l'))
+            return redirect(url_for('my_leave_requests'))
+    return redirect(url_for('login'))
 
-@app.route('/mlist', methods=['GET'])
-def mlist():
+
+@app.route('/my_leave_requests', methods=['GET'])
+def my_leave_requests():
     if "user" in session:
-        if len((w := "<ul>" + "".join(f'<li>{r[1]} - AT: {r[3]} REASON: {r[2]}</li>' for r in sqlite3.connect("database.db").cursor().execute('SELECT * FROM leave WHERE username = ?',(session["user"],)).fetchall()) + "</ul>")) != len('123456789'):
+        if len((w := "<ul>" + "".join(f'<li>{r[1]} - AT: {r[3]} REASON: {r[2]}</li>' for r in
+                                      sqlite3.connect("database.db").cursor().execute(
+                                              'SELECT * FROM leave WHERE username = ?',
+                                              (session["user"],)).fetchall()) + "</ul>")) != len('123456789'):
             return "<h1>Leave Requests</h1>" + w
         return "<h1>Leave Requests</h1>" + "<p>List empty</p>"
-    return redirect(url_for('l'))
+    return redirect(url_for('login'))
 
-@app.route('/list', methods=['GET'])
-def list():
+
+@app.route('/leave_requests', methods=['GET'])
+def leave_requests():
     if "user" in session:
-        if len((w := "<ul>" + "".join(f'<li>{r[1]} - AT: {r[3]} REASON: {r[2]}</li>' for r in sqlite3.connect("database.db").cursor().execute('SELECT * FROM leave WHERE username != ?', (session["user"],)).fetchall()) + "</ul>")) != len('123456789'):
+        if len((w := "<ul>" + "".join(f'<li>{r[1]} - AT: {r[3]} REASON: {r[2]}</li>' for r in
+                                      sqlite3.connect("database.db").cursor().execute(
+                                              'SELECT * FROM leave WHERE username != ?',
+                                              (session["user"],)).fetchall()) + "</ul>")) != len('123456789'):
             return "<h1>Leave Requests</h1>" + w
         return "<h1>Leave Requests</h1>" + "<p>List empty</p>"
-    return redirect(url_for('l'))
+    return redirect(url_for('login'))
+
 
 @app.route('/users', methods=['GET'])
 def users():
-    if len((w := "<ul>" + "".join(f'<li>{r[1]}</li>' for r in sqlite3.connect("database.db").cursor().execute('SELECT * FROM users').fetchall()) + "</ul>")) != len('123456789'):
+    if len((w := "<ul>" + "".join(f'<li>{r[1]}</li>' for r in sqlite3.connect("database.db").cursor().execute(
+            'SELECT * FROM users').fetchall()) + "</ul>")) != len('123456789'):
         return "<h1>Users</h1>" + w
     return "<h1>Users</h1>" + "<p>List empty</p>"
-        
+
 
 if __name__ == '__main__':
     app.run(debug=True)
